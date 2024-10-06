0f79b0bc99b1b60ccf72c1ec5b09a05dbb5818d7
795869 <hariram.b.2018.mech@rajalakshmi.edu.in>
2024-10-06 22:57:15 +0530

    Implemented Cloud-Config in all services

--- APIGateway/src/main/java/com/eCommerce/APIGateway/Routes.java
+++ APIGateway/src/main/java/com/eCommerce/APIGateway/Routes.java
@@ -18,48 +18,70 @@ public class Routes {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                // User Service Route
                 .route("user-service", r -> r.path("/api/v1/user/**", "/api/address/**")
                         .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("userServiceCircuitBreaker")
                                         .setFallbackUri("forward:/userServiceFallback")))
                         .uri("lb://USER-SERVICE"))
+
+                // Cart Service Route
                 .route("cart-service", r -> r.path("/api/cart/**")
                         .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("cartServiceCircuitBreaker")
                                         .setFallbackUri("forward:/cartServiceFallback")))
                         .uri("lb://CART-SERVICE"))
+
+                // Inventory Service Route
                 .route("inventory-service", r -> r.path("/api/inventory/**", "/api/products/**")
                         .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("inventoryServiceCircuitBreaker")
                                         .setFallbackUri("forward:/inventoryServiceFallback")))
                         .uri("lb://INVENTORY-SERVICE"))
+
+                // Order Service Route
                 .route("order-service", r -> r.path("/api/order/**")
                         .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("orderServiceCircuitBreaker")
                                         .setFallbackUri("forward:/orderServiceFallback")))
                         .uri("lb://ORDER-SERVICE"))
-                .route("notification-service", r -> r.path("/api/order/**")
+
+                // Notification Service Route
+                .route("notification-service", r -> r.path("/api/notification/**")
                         .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("notificationServiceCircuitBreaker")
                                         .setFallbackUri("forward:/notificationServiceFallback")))
                         .uri("lb://NOTIFICATION-SERVICE"))
+
+                // Security Service Route
                 .route("security-service", r -> r.path("/api/security/**")
                         .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("securityServiceCircuitBreaker")
                                         .setFallbackUri("forward:/securityServiceFallback")))
                         .uri("lb://SECURITY-SERVICE"))
+
+                // Wishlist Service Route
                 .route("wishlist-service", r -> r.path("/api/wishlist/**")
                         .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("wishlistServiceCircuitBreaker")
                                         .setFallbackUri("forward:/wishlistServiceFallback")))
                         .uri("lb://WISHLIST-SERVICE"))
+
+                .route("swagger_user_service", r -> r.path("/api-docs/user")
+                        .uri("http://localhost:8081/api-docs"))
+                .route("swagger_address_service", r -> r.path("/api-docs/address")
+                        .uri("http://localhost:8082/api-docs"))
+                .route("swagger_cart_service", r -> r.path("/api-docs/cart")
+                        .uri("http://localhost:8083/api-docs"))
+                .route("swagger_inventory_service", r -> r.path("/api-docs/inventory")
+                        .uri("http://localhost:8084/api-docs"))
                 .build();
     }
 }
